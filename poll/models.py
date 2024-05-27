d534dd61931c2cd8f26f4f2df4a05684644d1f18
NoeNavarro1 <noenavarroacevedo92@gmail.com>
2024-05-27 13:51:33 -0600

    5commit

--- poll/models.py
+++ poll/models.py
@@ -13,6 +13,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=100)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     votes = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.choice_text
--- poll/views.py
+++ poll/views.py
@@ -1,21 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Question
+
 # Create your views here.
 def home(request):
     questions = Question.objects.all()
-    return render(
+    return render( 
         request, 
         'poll/home.html',
         {
             "questions": questions
-        })
+         })
 
 def vote(request, q_id):
-    q = Question.objects.get(pk=q_id)
-    return render(request, 'poll/vote.html', {
-        "question": q
-    })
+    q = get_object_or_404(Question, pk=q_id)
+    if request.method == "POST":
+        choice_id = request.POST.get('choice')
+        choice = q.choice_set.get(pk=choice_id)
+        choice.votes += 1
+        choice.save()
+        return redirect('poll:result', q_id)
+    return render(
+        request, 
+        'poll/vote.html', 
+        {
+            "question": q
+         })
 
 def result(request, q_id):
-    pass
+    try:
+        q = Question.objects.get(pk=q_id)
+    except Question.DoesNotExist:
+        return redirect('poll:home')
+    return render(request, 'poll/result.html', {
+        "question": q
+    })
