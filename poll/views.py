ee5cf5d022f16e37d0eb7071d2cd2385f0e0a1c2
NoeNavarro1 <noenavarroacevedo92@gmail.com>
2024-06-02 00:21:17 -0600

    Creando graficas

--- poll/views.py
+++ poll/views.py
@@ -1,31 +1,37 @@
 from django.shortcuts import get_object_or_404, redirect, render
-
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
+
+
 def home(request):
     questions = Question.objects.all()
-    return render( 
-        request, 
+    return render(
+        request,
         'poll/home.html',
         {
             "questions": questions
-         })
+        })
+
 
 def vote(request, q_id):
     q = get_object_or_404(Question, pk=q_id)
     if request.method == "POST":
-        choice_id = request.POST.get('choice')
-        choice = q.choice_set.get(pk=choice_id)
-        choice.votes += 1
-        choice.save()
-        return redirect('poll:result', q_id)
-    return render(
-        request, 
-        'poll/vote.html', 
-        {
-            "question": q
-         })
+        try:
+            choice_id = request.POST.get('choice')
+            choice = q.choice_set.get(pk=choice_id)
+            choice.votes += 1
+            choice.save()
+            return redirect('poll:result', q_id)
+        except (KeyError, Choice.DoesNotExist):
+            return render(request, 'poll/vote.html', {
+                "question": q,
+                "error_message": "Debes elegir algo"
+            })
+    return render(request, 'poll/vote.html', {
+        "question": q
+    })
+
 
 def result(request, q_id):
     try:
